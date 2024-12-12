411527970e6af32cacfc98c4fb34173c47375966
Alejandro Vasconcellos <vasconalex17@gmail.com>
2024-12-12 23:44:47 +0000

    day2 part1

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,8 +1,51 @@
-use std::fs;
+use std::{fs, i32};
 
-pub fn run_part_1() -> String {
-    let contents =
-        fs::read_to_string("src/day1/input.txt").expect("Should have been able to read the file");
+pub fn run_part_1() -> u32 {
+    let lines =
+        fs::read_to_string("src/day2/input.txt").expect("Should have been able to read the file");
 
-    contents
+    let mut safe_reports: u32 = 0;
+
+    for line in lines.trim().split("\n") {
+        let reports: Vec<i32> = line
+            .split(|c: char| !c.is_numeric())
+            .filter_map(|s| s.parse::<i32>().ok())
+            .collect();
+
+        let mut is_decreasing: bool = false;
+        let mut is_increasing: bool = false;
+        let mut is_safe: bool = true;
+
+        for (i, report) in reports.iter().enumerate() {
+            if i + 1 == reports.len() {
+                break;
+            }
+
+            let diff: u32 = report.abs_diff(reports[i + 1]);
+
+            if diff == 0 || diff > 3 {
+                is_safe = false;
+            }
+
+            if *report < reports[i + 1] {
+                is_increasing = true;
+            } else {
+                is_decreasing = true;
+            }
+        }
+
+        if is_decreasing && is_increasing {
+            is_safe = false;
+        }
+
+        if is_safe {
+            safe_reports += 1;
+        }
+    }
+
+    safe_reports
+}
+
+pub fn run_part_2() {
+    todo!()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use aoc::day1;
+use aoc::day2;
 use aoc::measure_time;
 use std::env;
 use std::time::Instant;
@@ -11,6 +12,11 @@ fn main() {
             println!("Running day1...");
             measure_time!({ day1::run_part_1() }, 1, 1);
             measure_time!({ day1::run_part_2() }, 1, 2);
+        }
+        Some(arg) if arg == "day2" => {
+            println!("Running day2...");
+            measure_time!({ day2::run_part_1() }, 2, 1);
+            measure_time!({ day2::run_part_2() }, 2, 2);
         }
         _ => {
             println!("No day mod file found!");
