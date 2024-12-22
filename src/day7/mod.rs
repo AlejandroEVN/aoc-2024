3e1b8b0e81c8dbb37f0e1844c46d2b3aa911e4c2
AlejandroEVN <vasconalex17@gmail.com>
2024-12-22 17:10:44 +0100

    DAY 7 part 2 works!!

--- src/day7/mod.rs
+++ src/day7/mod.rs
@@ -15,7 +15,7 @@ pub fn part_1() -> i64 {
         match equation.first() {
             None => continue,
             Some(target) => {
-                let is_solvable = solve(*target, &equation[1..], 0);
+                let is_solvable = solve(*target, &equation[1..], 0, false);
                 if is_solvable {
                     total_calibration += target;
                 }
@@ -26,7 +26,7 @@ pub fn part_1() -> i64 {
     total_calibration
 }
 
-fn solve(target: i64, nums: &[i64], acc: i64) -> bool {
+fn solve(target: i64, nums: &[i64], acc: i64, allow_concat: bool) -> bool {
     match nums.first() {
         None => {
             if target == acc {
@@ -35,11 +35,36 @@ fn solve(target: i64, nums: &[i64], acc: i64) -> bool {
             return false;
         }
         Some(num) => {
-            return solve(target, &nums[1..], acc + num) || solve(target, &nums[1..], acc * num);
+            let concated_nums: i64 = (acc.to_string() + &num.to_string()).parse().unwrap();
+            return solve(target, &nums[1..], acc + num, allow_concat)
+                || solve(target, &nums[1..], acc * num, allow_concat)
+                || allow_concat && solve(target, &nums[1..], concated_nums, allow_concat);
         }
     }
 }
 
-pub fn part_2() -> usize {
-    0
+pub fn part_2() -> i64 {
+    let data = include_str!("input.txt");
+
+    let regex = Regex::new(r"[^\d]+").unwrap();
+
+    let mut total_calibration = 0;
+    for line in data.lines() {
+        let equation: Vec<i64> = regex
+            .split(line)
+            .filter_map(|str| str.parse().ok())
+            .collect();
+
+        match equation.first() {
+            None => continue,
+            Some(target) => {
+                let is_solvable = solve(*target, &equation[1..], 0, true);
+                if is_solvable {
+                    total_calibration += target;
+                }
+            }
+        }
+    }
+
+    total_calibration
 }
